475c9860279e841193c02c94f8c8f971673c5e40
Tim Eisler <TimEisler@outlook.com>
2022-01-06 15:56:21 -0600

    Created Site class and wrote ssg to use it.

---
+++ ssg/site.py
@@ -0,0 +1,18 @@
+from os import mkdir
+from pathlib import Path
+
+
+class Site:
+    def __init__(self, source, dest):
+        self.source=Path(source)
+        self.dest=Path(dest)
+
+    def create_dir(self, path):
+        directory = self.dest / self.source.relative_to(path)
+        mkdir(directory, parents=True, exist_ok=True)
+
+    def build(self):
+        mkdir(self.dest, parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if Path(path).is_dir():
+                self.create_dir(path)
